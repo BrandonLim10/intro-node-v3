c414486d76a86fef3332ff4e85b1505df0548e1f
Brandon Lim <brandonlim10p@icloud.com>
2023-11-10 14:23:33 -0800

    commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+#!/usr/bin/env node
+import fs from 'fs'
+import http from 'http'
+
+const note = process.argv[2]
+const newNote = {
+    content: note,
+    id: Date.now()
+}
