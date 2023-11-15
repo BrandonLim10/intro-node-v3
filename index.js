4d6876452160253cacd7500818a1aa7440320ba4
Brandon Lim <brandonlim10p@icloud.com>
2023-11-15 14:25:00 -0800

    gitignore:

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
-import fs from 'fs'
-import http from 'http'
 
-const note = process.argv[2]
-const newNote = {
-    content: note,
-    id: Date.now()
-}
+import './src/command.js'
